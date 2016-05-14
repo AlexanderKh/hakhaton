f6f53c5178e6528f8a54884d6886d11a8798f3db
Alexander Khomchenko <alexanderkh@interlink-ua.com>
2016-05-14 18:49:17 +0200

    implement update problem function

--- models/m_problems.php
+++ models/m_problems.php
@@ -24,3 +24,12 @@ function create_problem ($title, $description, $category_id) {
 	$sql = "INSERT INTO problems(title, description, category_id, approved, creation_date, user_id) VALUES('$title', '$description', '$category_id', 1, NOW(), 1)";
 	mysql_query($sql);
 }
+
+function update_problem ($id, $title, $description, $category_id) {
+	$sql = "UPDATE problems 
+			SET title='$title', 
+				description='$description', 
+				category_id='$category_id' 
+			WHERE id='$id'";
+	mysql_query($sql);
+}
