4a6571dc58529a7bc153452ef97187a2aa8872e1
flyer <datus3r@gmail.com>
2016-05-15 06:00:55 +0300

    dsdsds

---
+++ users.php
@@ -0,0 +1,41 @@
+<?php
+include_once('models/m_startup.php');
+include_once('models/m_problems.php');
+include_once('models/m_users.php');
+include_once('models/m_main_functions.php');
+include_once('models/m_categories.php');
+
+startup();
+
+$problems=get_approved_problems();
+
+$allcategories=get_all_categories();
+
+$title="Головна";
+
+$parameter='pr';
+
+if ($_GET['mode'] == 'reg'){
+    $content=view_include(
+        'views/v_reg_user.php',
+    array());
+
+    $page = view_include(
+        'views/v_main.php',
+        array('title' => $title, 'content' => $content, 'parameter' => $parameter, 'allcategories' => $allcategories));
+
+    goto a;
+}
+
+$content = view_include(
+    'views/v_index.php',
+    array('problems' => $problems, 'is_admin' => false));
+
+$page = view_include(
+    'views/v_main.php',
+    array('title' => $title, 'content' => $content, 'parameter' => $parameter, 'allcategories' => $allcategories));
+
+a:
+echo $page;
+
+?>
---
+++ views/v_reg_user.php
@@ -0,0 +1,21 @@
+<div class="content">
+    <div class="new">
+<form action="save_user.php" method="post">
+<!--**** save_user.php - это адрес обработчика. То есть, после нажатия на кнопку "Зарегистрироваться", данные из полей отправятся на страничку save_user.php методом "post" ***** -->
+
+    <label>Ваш логин:*</br></label>
+    <p> <input name="login" type="text" size="15" maxlength="15">
+  </p>
+<!--**** В текстовое поле (name="login" type="text") пользователь вводит свой логин ***** -->
+
+    <label>Ваш пароль:<br></label>
+    <p>
+    <input name="password" type="password" size="15" maxlength="15">
+  </p>
+<!--**** В поле для паролей (name="password" type="password") пользователь вводит свой пароль ***** -->
+<p>
+<input type="submit" name="submit" value="Зарегистрироваться">
+<!--**** Кнопочка (type="submit") отправляет данные на страничку save_user.php ***** -->
+</p></form>
+    </div>
+</div>
